2b94e77b85f67491f55b925f9f4831e918cbbab9
remix <remix@ubuntu.(none)>
2013-01-21 16:42:50 -0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bellydanceuruguay::Application.config.secret_token = 'fe75c9af924b3122efb60bfff31c9d2563a5768444ee5d1e8a9120683244539dc0e08c6250768e058bfa4e96b77e8412b03c5325a91ca619b03911d5bde864d2'
